8e7572ca5874beeae174ad4da4d015524b8f600a
Chris <chriskrol97@gmail.com>
2021-03-13 13:08:52 -0500

    Added search.js file and minor tweeks to other files

---
+++ search.js
@@ -0,0 +1,14 @@
+
+document.getElementById("search").addEventListener("click", moviesearch);
+
+function getResults(){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState==4 && this.status==200){
+
+        }
+    };
+
+    xhttp.open("GET", "http://localhost:3000/searchResults?", true);
+    xhttp.send();
+}
